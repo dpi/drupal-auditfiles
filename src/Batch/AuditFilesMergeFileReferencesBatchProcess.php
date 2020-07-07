3adddfe054ed6545c63dc25e1c9e5c1a75599d8d
Lisa Ridley <lisa@codementality.com>
2020-07-07 15:12:37 -0400

    Refactoring AuditFilesMergeFileReferencesBatchProcess to make it testable

--- src/Batch/AuditFilesMergeFileReferencesBatchProcess.php
+++ src/Batch/AuditFilesMergeFileReferencesBatchProcess.php
@@ -4,6 +4,7 @@ namespace Drupal\auditfiles\Batch;
 
 use Drupal\Component\Utility\Html;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\auditfiles\ServiceAuditFilesMergeFileReferences;
 
 /**
  * Process batch files.
@@ -11,6 +12,43 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  * @todo Refactor to make a Factory Worker class.
  */
 class AuditFilesMergeFileReferencesBatchProcess {
+
+  /**
+   * The File entity ID to delete.
+   *
+   * @var int
+   */
+  protected $fileId;
+
+  /**
+   * The File entity IDs to merge.
+   *
+   * @var array
+   */
+  protected $fileIds;
+
+  /**
+   * ManagedNotUsed service.
+   *
+   * @var \Drupal\auditfiles\ServiceAuditFilesMergeFileReferences
+   */
+  protected $mergeFileReferences;
+
+  /**
+   *  Class constructor.
+   *
+   * @param \Drupal\auditfiles\ServiceAuditFilesMergeFileReferences $merge_file_references
+   *   Injected ServiceAuditFilesManagedNotUsed service.
+   * @param int $file_id
+   *   File entity ID to delete.
+   * @param array $files_being_merged
+   *   File IDs to merge.
+   */
+  public function __construct(ServiceAuditFilesMergeFileReferences $merge_file_references, $file_being_kept, array $files_being_merged) {
+    $this->mergeFileReferences = $merge_file_references;
+    $this->fileId = $file_being_kept;
+    $this->fileIds = $files_being_merged;
+  }
 
   /**
    * The batch process for deleting the file.
@@ -27,8 +65,24 @@ class AuditFilesMergeFileReferencesBatchProcess {
    * a factory worker on that service.
    */
   public static function auditfilesMergeFileReferencesBatchMergeProcessBatch($file_being_kept, $file_being_merged, array &$context) {
-    \Drupal::service('auditfiles.merge_file_references')->auditfilesMergeFileReferencesBatchMergeProcessFile($file_being_kept, $file_being_merged);
-    $context['results'][] = $file_being_merged;
+    $mergeFileReferences = \Drupal::service('auditfiles.merge_file_references');
+    $worker = new static($mergeFileReferences, $file_being_kept, $file_being_merged);
+    $worker->dispatch($context);
+  }
+
+  /**
+   * Processes the file IDs to delete and merge.
+   *
+   * @param \Drupal\auditfiles\ServiceAuditFilesMergeFileReferences $merge_file_references
+   *   Injected ServiceAuditFilesManagedNotUsed service.
+   * @param int $file_being_kept
+   *   File entity ID to delete.
+   * @param array $files_being_merged
+   *   File IDs to merge.
+   */
+  protected function dispatch(&$context) {
+    $this->mergeFileReferences->auditfilesMergeFileReferencesBatchMergeProcessFile($file_being_kept, $file_being_merged);
+    $context['results'][] = Html::escape($file_being_merged);
     $context['message'] = new TranslatableMarkup(
       'Merged file ID %file_being_merged into file ID %file_being_kept.',
       [
